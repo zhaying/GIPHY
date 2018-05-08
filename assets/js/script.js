0118a416fb5e9d2f790ba71dfdb498d534b9343c
zhaying <8896093+zhaying@users.noreply.github.com>
2018-05-08 16:59:08 -0400

    changed api to https

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,7 @@ function createJQcloudFormattedData(anArrayList) {
         text: item,
         weight: faker.random.number({min:1, max:10}),
         handlers: { click: function() {
-          let url = "http://api.giphy.com/v1/gifs/search?q=" + item + "&api_key=xpk7qtWFTosq9WPfU94mFoqXyCSGmSj9&limit=10";
+          let url = "https://api.giphy.com/v1/gifs/search?q=" + item + "&api_key=xpk7qtWFTosq9WPfU94mFoqXyCSGmSj9&limit=10";
           var xhr = $.get(url);
           xhr.done(function(results) {
             console.log("success got results", results);
